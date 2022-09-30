efceeebe8ed4879c216056b2a60fbd27c72b9924
Florian Hauenstein <hauenst@jlab.org>
2022-09-30 14:13:20 -0400

    * DC residual histogram, cosmetical changes to binning and x-range

--- src/main/java/org/jlab/clas12/monitoring/tof_monitor.java
+++ src/main/java/org/jlab/clas12/monitoring/tof_monitor.java
@@ -291,25 +291,25 @@ public class tof_monitor {
 			p2_tdcadc_dt[s].setTitleY("counts"); 
 
 			for(int sl=0;sl<6;sl++){
-				DC_residuals_trkDoca[s][sl] = new H2F(String.format("DC_residuals_trkDoca_%d_%d",s+1,sl+1),String.format("DC_residuals_trkDoca_%d_%d",s+1,sl+1),100,0,DCcellsizeSL[sl],100,-1,1);
+				DC_residuals_trkDoca[s][sl] = new H2F(String.format("DC_residuals_trkDoca_%d_%d",s+1,sl+1),String.format("DC_residuals_trkDoca_%d_%d",s+1,sl+1),100,0,DCcellsizeSL[sl],400,-0.5,0.5);
 				DC_residuals_trkDoca[s][sl].setTitle(String.format("DC residuals S%d SL%d",s+1,sl+1));
 				DC_residuals_trkDoca[s][sl].setTitleX("DOCA (cm)");
 				DC_residuals_trkDoca[s][sl].setTitleY("residual (cm)");
-				DC_residuals_trkDoca_nocut[s][sl] = new H2F(String.format("DC_residuals_trkDoca_nocut_%d_%d",s+1,sl+1),String.format("DC_residuals_trkDoca_nocut_%d_%d",s+1,sl+1),100,0,DCcellsizeSL[sl],100,-1,1);
+				DC_residuals_trkDoca_nocut[s][sl] = new H2F(String.format("DC_residuals_trkDoca_nocut_%d_%d",s+1,sl+1),String.format("DC_residuals_trkDoca_nocut_%d_%d",s+1,sl+1),100,0,DCcellsizeSL[sl],400,-0.5,0.5);
 				DC_residuals_trkDoca_nocut[s][sl].setTitle(String.format("DC residuals S%d SL%d",s+1,sl+1));
 				DC_residuals_trkDoca_nocut[s][sl].setTitleX("DOCA (cm)");
 				DC_residuals_trkDoca_nocut[s][sl].setTitleY("residual (cm)");
-				DC_residuals_trkDoca_rescut[s][sl] = new H2F(String.format("DC_residuals_trkDoca_rescut_%d_%d",s+1,sl+1),String.format("DC_residuals_trkDoca_rescut_%d_%d",s+1,sl+1),100,0,DCcellsizeSL[sl],100,-0.5,0.5);
+				DC_residuals_trkDoca_rescut[s][sl] = new H2F(String.format("DC_residuals_trkDoca_rescut_%d_%d",s+1,sl+1),String.format("DC_residuals_trkDoca_rescut_%d_%d",s+1,sl+1),100,0,DCcellsizeSL[sl],400,-0.5,0.5);
 				DC_residuals_trkDoca_rescut[s][sl].setTitle(String.format("DC residuals S%d SL%d",s+1,sl+1));
 				DC_residuals_trkDoca_rescut[s][sl].setTitleX("DOCA (cm)");
 				DC_residuals_trkDoca_rescut[s][sl].setTitleY("residual (cm)");
-				DC_residuals[s][sl] = new H1F(String.format("DC_residuals_%d_%d",s+1,sl+1),String.format("DC_residuals_%d_%d",s+1,sl+1),100,-1,1);
+				DC_residuals[s][sl] = new H1F(String.format("DC_residuals_%d_%d",s+1,sl+1),String.format("DC_residuals_%d_%d",s+1,sl+1),400,-0.5,0.5);
 				DC_residuals[s][sl].setTitle(String.format("DC residuals S%d SL%d",s+1,sl+1));
 				DC_residuals[s][sl].setTitleX("residual (cm)");
-				DC_residuals_nocut[s][sl] = new H1F(String.format("DC_residuals_nocut_%d_%d",s+1,sl+1),String.format("DC_residuals_nocut_%d_%d",s+1,sl+1),100,-1,1);
+				DC_residuals_nocut[s][sl] = new H1F(String.format("DC_residuals_nocut_%d_%d",s+1,sl+1),String.format("DC_residuals_nocut_%d_%d",s+1,sl+1),400,-0.5,0.5);
 				DC_residuals_nocut[s][sl].setTitle(String.format("DC residuals S%d SL%d",s+1,sl+1));
 				DC_residuals_nocut[s][sl].setTitleX("residual (cm)");
-				DC_residuals_rescut[s][sl] = new H1F(String.format("DC_residuals_rescut_%d_%d",s+1,sl+1),String.format("DC_residuals_rescut_%d_%d",s+1,sl+1),100,-0.5,0.5);
+				DC_residuals_rescut[s][sl] = new H1F(String.format("DC_residuals_rescut_%d_%d",s+1,sl+1),String.format("DC_residuals_rescut_%d_%d",s+1,sl+1),400,-0.5,0.5);
 				DC_residuals_rescut[s][sl].setTitle(String.format("DC residuals S%d SL%d",s+1,sl+1));
 				DC_residuals_rescut[s][sl].setTitleX("residual (cm)");
 				DC_time[s][sl] = new H1F(String.format("DC_Time_%d_%d",s+1,sl+1),String.format("DC_Time_%d_%d",s+1,sl+1),200,-100,1000);
